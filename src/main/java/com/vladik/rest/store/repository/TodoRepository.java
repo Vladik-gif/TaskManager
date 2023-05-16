15dc62b03b46622f7385b5cbd5643f73b02f2c09
Vladik <v4054741@gmail.com>
2023-05-16 19:10:49 +0300

    minor fixes

--- src/main/java/com/vladik/rest/store/repository/TodoRepository.java
+++ src/main/java/com/vladik/rest/store/repository/TodoRepository.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-
     Optional<TodoEntity> findByTitle (String todo);
 }
