a4b30e94b1ae31dbd10a175fc392523ba230809a
Vladik <v4054741@gmail.com>
2024-01-02 15:29:10 +0300

    Added oauth2-client (GitHub, Google)

--- src/main/java/com/vladik/rest/api/config/SecurityConfig.java
+++ src/main/java/com/vladik/rest/api/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.vladik.rest.store.entities.RoleEntity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -35,7 +36,9 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
