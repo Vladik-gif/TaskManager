29b02c89ce351051f5a5254d010b398e16db6e7b
Vladik <v4054741@gmail.com>
2024-01-04 15:12:25 +0300

    entity fix

--- src/main/java/com/vladik/rest/store/entities/UserEntity.java
+++ src/main/java/com/vladik/rest/store/entities/UserEntity.java
@@ -6,8 +6,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
