fa2fceb9b374b084c8a9252cfb94d3014338b602
Vladik <v4054741@gmail.com>
2024-01-06 12:55:59 +0300

    Config and updated the security class

---
+++ src/main/java/com/vladik/rest/api/config/PasswordEncoderProvider.java
@@ -0,0 +1,14 @@
+package com.vladik.rest.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoderProvider {
+    @Bean
+    protected PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+}
