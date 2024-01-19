f6b20423d5a77c879005e3b462f6c3469ebaa34b
Vladik <v4054741@gmail.com>
2024-01-19 13:22:39 +0300

    Update Controller Security

--- src/main/java/com/vladik/rest/api/controller/CurrentController.java
+++ src/main/java/com/vladik/rest/api/controller/CurrentController.java
@@ -1,11 +1,8 @@
 package com.vladik.rest.api.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -14,25 +11,8 @@ public class CurrentController {
     private static final String CURRENT_AUTH = "/current";
 
     @GetMapping(CURRENT_AUTH)
-    public String getCurrentUser() {
-        // Отримати поточний контекст безпеки
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (authentication != null && authentication.isAuthenticated()) {
-            // Отримати деталі користувача
-            Object principal = authentication.getPrincipal();
-
-            if (principal instanceof UserDetails userDetails) {
-                String username = userDetails.getUsername();
-                return "Current User: " + username;
-            } else {
-                // Якщо principal - це не UserDetails, можна використовувати principal.toString()
-                String username = principal.toString();
-                return "Current User: " + username;
-            }
-        } else {
-            // Користувач не аутентифікований
-            return "User not authenticated";
-        }
+    @ResponseBody
+    public String currentUser(@AuthenticationPrincipal UserDetails userDetails){
+        return "User Details: " + userDetails.getUsername();
     }
 }
